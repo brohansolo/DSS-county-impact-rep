e1a9c0c41caf08ad9bc71f3426e2d81de771f1ea
rohan <rohanbansal406@gmail.com>
2020-11-15 00:49:53 -0600

    imported new files

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import maps
+import findings
 import streamlit as st
 
 st.title("County Impact for Covid-19")
@@ -5,8 +7,8 @@ st.subheader("Made for DSS Datathon")
 
 
 PAGES = {
-        "County Stats through Maps": maps.py,
-        "Methodology and Findings": findings.py,
+        "County Stats through Maps": maps,
+        "Methodology and Findings": findings
         }
 
 st.sidebar.title('Navigation')
