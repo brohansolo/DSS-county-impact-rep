b21ab94f22d941aa2c97037aa11c384c524d842f
rohan <rohanbansal406@gmail.com>
2020-11-14 23:39:12 -0600

    some more stuff

--- app.py
+++ app.py
@@ -2,3 +2,15 @@ import streamlit as st
 
 st.title("County Impact for Covid-19")
 st.subheader("Made for DSS Datathon")
+
+
+PAGES = {
+        "County Stats": ,
+        "Assessment Metrics":
+        "Methodology and Findings": ,
+        "About Us": 
+        }
+
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()));
+page = PAGES[selection];
